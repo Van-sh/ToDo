2a72ae6580ec7884e5d452f703e2e2bcd9836ef0
Van-sh <72124535+Van-sh@users.noreply.github.com>
2024-09-07 18:31:33 +0530

    Add prettier for uniform tab spaces

--- src/App.jsx
+++ src/App.jsx
@@ -6,76 +6,76 @@ import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 function pushToLocalStorage(key, object) {
-    localStorage.setItem(key, JSON.stringify(object));
+   localStorage.setItem(key, JSON.stringify(object));
 }
 
 function pullFromLocalStorage(key) {
-    let data = JSON.parse(localStorage.getItem(key));
-    if (data === null) {
-        data = [];
-        pushToLocalStorage(key, data);
-    }
-    console.log(data)
-    return data;
+   let data = JSON.parse(localStorage.getItem(key));
+   if (data === null) {
+      data = [];
+      pushToLocalStorage(key, data);
+   }
+   console.log(data);
+   return data;
 }
 
 function App() {
-    const [todoList, setTodoList] = useState(pullFromLocalStorage("todoList"));
+   const [todoList, setTodoList] = useState(pullFromLocalStorage("todoList"));
 
-    function handleTodoCreation(newTodo) {
-        newTodo = { id: uuidv4(), ...newTodo };
-        let newTodoList = [...todoList, newTodo];
-        pushToLocalStorage("todoList", newTodoList);
-        setTodoList(newTodoList);
-    }
+   function handleTodoCreation(newTodo) {
+      newTodo = { id: uuidv4(), ...newTodo };
+      let newTodoList = [...todoList, newTodo];
+      pushToLocalStorage("todoList", newTodoList);
+      setTodoList(newTodoList);
+   }
 
-    function handleTodoCompletion(id) {
-        let newTodoList = todoList;
-        newTodoList.map((todo) => {
-            if (todo.id === id) {
-                todo.isDone = !todo.isDone;
-            }
-        });
+   function handleTodoCompletion(id) {
+      let newTodoList = todoList;
+      newTodoList.map((todo) => {
+         if (todo.id === id) {
+            todo.isDone = !todo.isDone;
+         }
+      });
 
-        pushToLocalStorage("todoList", newTodoList);
-        setTodoList(newTodoList);
-    }
+      pushToLocalStorage("todoList", newTodoList);
+      setTodoList(newTodoList);
+   }
 
-    function handleTodoDeletionById(id) {
-        let newTodoList = todoList.filter((todo) => todo.id != id);
-        pushToLocalStorage("todoList", newTodoList);
-        setTodoList(newTodoList);
-    }
-    function handleTodoDeletion(isDone) {
-        let newTodoList = todoList.filter((todo) => isDone && !todo.isDone);
-        pushToLocalStorage("todoList", newTodoList);
-        setTodoList(newTodoList);
-    }
+   function handleTodoDeletionById(id) {
+      let newTodoList = todoList.filter((todo) => todo.id != id);
+      pushToLocalStorage("todoList", newTodoList);
+      setTodoList(newTodoList);
+   }
+   function handleTodoDeletion(isDone) {
+      let newTodoList = todoList.filter((todo) => isDone && !todo.isDone);
+      pushToLocalStorage("todoList", newTodoList);
+      setTodoList(newTodoList);
+   }
 
-    return (
-        <>
-            <main className="bg-base-300 rounded-xl flex flex-col items-center">
-                <h1 className="text-5xl mb-3">Todos</h1>
-                <TodoControls
-                    handleTodoCreation={handleTodoCreation}
-                    handleTodoDeletion={handleTodoDeletion}
-                ></TodoControls>
-                {(todoList.length > 0) ? (
-                    <>
-                        <div className="divider"></div>
-                        <TodoList
-                            todoList={todoList}
-                            handleTodoCompletion={handleTodoCompletion}
-                            handleTodoDeletionById={handleTodoDeletionById}
-                        ></TodoList>
-                    </>
-                ) : (
-                    <></>
-                )}
-            </main>
-            <ThemeController></ThemeController>
-        </>
-    );
+   return (
+      <>
+         <main className="bg-base-300 rounded-xl flex flex-col items-center">
+            <h1 className="text-5xl mb-3">Todos</h1>
+            <TodoControls
+               handleTodoCreation={handleTodoCreation}
+               handleTodoDeletion={handleTodoDeletion}
+            ></TodoControls>
+            {todoList.length > 0 ? (
+               <>
+                  <div className="divider"></div>
+                  <TodoList
+                     todoList={todoList}
+                     handleTodoCompletion={handleTodoCompletion}
+                     handleTodoDeletionById={handleTodoDeletionById}
+                  ></TodoList>
+               </>
+            ) : (
+               <></>
+            )}
+         </main>
+         <ThemeController></ThemeController>
+      </>
+   );
 }
 
 export default App;
--- src/components/TodoControls.jsx
+++ src/components/TodoControls.jsx
@@ -1,56 +1,54 @@
 import { useState } from "react";
 
 function TodoControls({ handleTodoCreation, handleTodoDeletion }) {
-    const [inputValue, setInputValue] = useState("");
+   const [inputValue, setInputValue] = useState("");
 
-    function todoCreation() {
-        handleTodoCreation({ text: inputValue, isDone: false });
-        setInputValue("");
-    }
+   function todoCreation() {
+      handleTodoCreation({ text: inputValue, isDone: false });
+      setInputValue("");
+   }
 
-    return (
-        <>
-            <div className="todo-controls flex flex-col gap-2">
-                <input
-                    className="input w-full input-bordered flex-grow-0 todo-input"
-                    type="text"
-                    placeholder="eg. Eat Food"
-                    autoFocus
-                    onChange={(event) =>
-                        setInputValue(event.target.value.trim())
-                    }
-                    onKeyDown={(event) => {
-                        if (event.key === "Enter" && inputValue != "") {
-                            todoCreation();
-                        }
-                    }}
-                    value={inputValue}
-                />
-                <div className="buttons flex gap-1 justify-start">
-                    <button
-                        className="btn btn-primary"
-                        onClick={() => {
-                            if (inputValue != "") todoCreation();
-                        }}
-                    >
-                        Add
-                    </button>
-                    <button
-                        className="btn btn-secondary"
-                        onClick={() => handleTodoDeletion(true)}
-                    >
-                        Clear Completed
-                    </button>
-                    <button
-                        className="btn btn-error"
-                        onClick={() => handleTodoDeletion(false)}
-                    >
-                        Clear All
-                    </button>
-                </div>
+   return (
+      <>
+         <div className="todo-controls flex flex-col gap-2">
+            <input
+               className="input w-full input-bordered flex-grow-0 todo-input"
+               type="text"
+               placeholder="eg. Eat Food"
+               autoFocus
+               onChange={(event) => setInputValue(event.target.value.trim())}
+               onKeyDown={(event) => {
+                  if (event.key === "Enter" && inputValue != "") {
+                     todoCreation();
+                  }
+               }}
+               value={inputValue}
+            />
+            <div className="buttons flex gap-1 justify-start">
+               <button
+                  className="btn btn-primary"
+                  onClick={() => {
+                     if (inputValue != "") todoCreation();
+                  }}
+               >
+                  Add
+               </button>
+               <button
+                  className="btn btn-secondary"
+                  onClick={() => handleTodoDeletion(true)}
+               >
+                  Clear Completed
+               </button>
+               <button
+                  className="btn btn-error"
+                  onClick={() => handleTodoDeletion(false)}
+               >
+                  Clear All
+               </button>
             </div>
-        </>
-    );
+         </div>
+      </>
+   );
 }
 
 export default TodoControls;
